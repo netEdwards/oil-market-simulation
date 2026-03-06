77bc6db8cf2ed24ed7bcf0d8a9ba86234364ce08
Adrian Edwards <aedwards0603@gmail.com>
2026-03-06 11:52:45 -0500

    Implemented small wtp generator in buyer class. - Buyer class now has its own wtp generation - Takes in mean wtp and deviation -- Clamped to _min and _max from class init params. - Not tested yet

--- src/oilmarket/agents/buyer.py
+++ src/oilmarket/agents/buyer.py
@@ -5,16 +5,32 @@ from dataclasses import dataclass
 from uuid import uuid4
 import uuid
 
+import numpy as np
 
-@dataclass
+
+
 class Buyer:
-    id: str = "NIB" # default is non-initiated buyer or nib. Used to filter out buyers that were created and not initiated for debugging
-    wtp: float = 0.0
-    avg_demand: float = 0.0
-    active: bool = False #is the buyer currently buying
-    
-    def __init__(self):
-        self.id = str(uuid4())
+    def __init__(
+        self, 
+        mean_wtp: float = 0.0, 
+        signma: float = 0.0, 
+        _min: float = 0.0, 
+        _max: float = 0.0, 
+        seed: str | int = None, 
+        active: bool = False, 
+        wtp: float = 0.0) -> None:
+        
+        self.id = str(uuid.uuid4())
+        self.rng = np.random.default_rng(seed=seed)
+        self.mean_wtp = mean_wtp
+        self.sigma = signma
+        self._min = _min
+        self._max = _max
+        self.seed = seed 
+        self.active = active
+        
+        self.wtp = self.generate_wtp()
+        
         
     
     def generate_demand(self):
@@ -26,3 +42,17 @@ class Buyer:
         """
         Method to select a seller from a given subset k sellers
         """
+        
+    def generate_wtp(self) -> float:
+        """
+        Function to generate the willingness to pay value for the simulation run. 
+        """
+        mu = self.mean_wtp
+        sigma = self.sigma
+        _min = self._min
+        _max = self._max
+        
+        while True:
+            x = self.rng.normal(mu, sigma) #generate one normal dist number from the mean and deviation
+            if _min <= x <= _max: #only return whats inside the min and max
+                return x
