475d951b8d37142500eb2a79a83fdd9a8b265c77
Adrian Edwards <aedwards0603@gmail.com>
2026-03-10 12:12:18 -0400

    Finished buyer demand implementation. - Used np.random.default_rng(seed) for all rng. - Used Poisson dist on buyer demand selection for stochastic-ness - Will postpon logging and demand unmet logging until after structure is finalized.

--- src/oilmarket/agents/buyer.py
+++ src/oilmarket/agents/buyer.py
@@ -18,6 +18,7 @@ class Buyer:
         _max: float = 0.0, 
         seed: str | int = None, 
         active: bool = False, 
+        lambda_demand: float = 4.0,
         wtp: float = 0.0) -> None:
         
         self.id = str(uuid.uuid4())
@@ -28,20 +29,26 @@ class Buyer:
         self._max = _max
         self.seed = seed 
         self.active = active
+        self.lambda_i = lambda_demand
         
         self.wtp = self.generate_wtp()
-        
+        self.demand = self.generate_demand()
         
     
     def generate_demand(self):
         """
         A method to generate a buyers demand using the Poisson random selection method.
         """
+        demand = self.rng.poisson(self.lambda_i)
+        return demand
+        
         
     def select_seller(self, k):
         """
         Method to select a seller from a given subset k sellers
         """
+        # for each seller in subset K is the price per unit lower than the wtp, then use demand to calculate how much they can buy from that seller. If they cannot buy all of there demand (due to supplier check and more) then demand is unmet. Next seller.
+        
         
     def generate_wtp(self) -> float:
         """
