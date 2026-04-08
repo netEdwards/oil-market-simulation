2d404b9e36e6575292be34e213acf22e3a21c62e
Adrian Edwards <aedwards0603@gmail.com>
2026-04-08 07:47:56 -0400

    Execution worker update: - Now supports analysis being complete. - New parameter: execution_result: ExecutionResult - New function: run_analysis() -> completes an analysis using the ExperimentAnalyzer - New Signal() -> "analysis_complete" emits a dict which is the analysis.

--- src/app/experiment_runner_controller.py
+++ src/app/experiment_runner_controller.py
@@ -1,5 +1,7 @@
 from numpy import isin
 
+from experiments.experiment import Experiment
+from experiments.experiment_analyzer import ExperimentAnalyzer
 from experiments.experiment_executer import ExperimentExecuter, ExecutionResult
 from PySide6.QtCore import QObject, Signal, QThread, Slot
 
@@ -15,14 +17,18 @@ class ExecutionWorker(QObject):
     failed = Signal(str)
     timestep_update = Signal(dict)
     execution_update = Signal(dict)
+    analysis_complete = Signal(dict)
     
-    def __init__(self, experiment: dict):
+    def __init__(self, experiment: dict | Experiment, execution_result: ExecutionResult = None):
         super().__init__()
         
         self.experiment = experiment
         self.run_control = RunController(
             tick_delay_ms=25
         )
+        self.execution_result = execution_result
+        
+        print("Execution worker initialized.")
 
     @Slot()
     def run(self):
@@ -61,7 +67,26 @@ class ExecutionWorker(QObject):
             
         except Exception as e:
             self.failed.emit(str(e))
+    
+    @Slot()
+    def run_analysis(self):        
+        if isinstance(self.experiment, dict):
+            self.experiment = Experiment.load(self.experiment)
+            
+        analysis_inst = ExperimentAnalyzer(
+            experiment=self.experiment,
+            execution_result=self.execution_result,
+        )
         
+        analysis = analysis_inst.build_all_analysis()
+        analysis_inst.save_analysis()
+        
+        if not isinstance(analysis, dict):
+            self.failed.emit("There was an error generating the analysis.")
+            raise Exception("There was an error generating the analysis.")
+
+        self.analysis_complete.emit(analysis)
+    
     @Slot()
     def request_skip(self):
         if self.run_control:
